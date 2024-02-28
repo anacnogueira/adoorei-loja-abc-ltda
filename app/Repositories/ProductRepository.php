652d22247f6ff1c2176762811728e0f4a6ce90c0
Ana Claudia <anacnogueira@gmail.com>
2024-02-28 11:38:55 -0300

    Cria Repositório de Produtos

---
+++ app/Repositories/ProductRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\ProductRepositoryInterface;
+use App\Models\Product;
+
+class ProductRepository implements ProductRepositoryInterface
+{
+    protected $product;
+
+    public function __construct(Product $product) {
+        $this->product = $product;
+    }
+    
+    
+    public function getAllProducts()
+    {   
+        $this->product->all();
+    }
+}
