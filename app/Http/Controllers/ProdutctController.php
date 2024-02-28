92e304bcdd80483d4d6973a41cfb6762e343600c
Ana Claudia <anacnogueira@gmail.com>
2024-02-28 08:51:43 -0300

    Cria todos os recursos para produtos

---
+++ app/Http/Controllers/ProdutctController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreProdutctRequest;
+use App\Http\Requests\UpdateProdutctRequest;
+use App\Models\Produtct;
+
+class ProdutctController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProdutctRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Produtct $produtct)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Produtct $produtct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProdutctRequest $request, Produtct $produtct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Produtct $produtct)
+    {
+        //
+    }
+}
