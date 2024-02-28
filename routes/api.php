f7e3f734746c31a74303ed570140bc3374461f86
Ana Claudia <anacnogueira@gmail.com>
2024-02-28 11:48:05 -0300

    Cria rota para listar produtos

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Products
+Route::get('products', [ProductController::class, 'index']);
 
 // Sales
 
